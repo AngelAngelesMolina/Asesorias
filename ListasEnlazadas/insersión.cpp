47d5fd22b0594942f0511e12f48bf748093bd346
Angel Angeles <angel.angeles.molina@gmail.com>
2023-05-22 19:45:10 -0600

    Eliminacion de listas

--- ListasEnlazadas/insersión.cpp
+++ ListasEnlazadas/insersión.cpp
@@ -63,13 +63,56 @@ void buscarElemento(Nodo *lista, int n){
     }
 
 }
+void eliminarLista(Nodo *&lista, int &n){ 
+// 1. Crear un nodo aux e igualarlo al inicio de la lista
+    Nodo *aux =  lista; 
+// 2. Guardar el data que queremos eliminar dentro de *aux
+    n = aux-> dato; 
+// 3. Pasar lista  asiguiente nodo 
+    lista = aux->siguiente; 
+// 4. Eliminar aux 
+    delete aux; 
 
+}
+
+void eliminarElemento(Nodo *&lista, int n){ 
+// 1. Preguntar si la lista no esta vacia 
+    if(lista != NULL){ 
+// 2. crear un *aux_borrar y *anterior=NULL 
+        Nodo *aux_borrar ;
+        Nodo *anterior = NULL;
+
+// 3. Igualar *aux_borrar al inicio de la lista 
+        aux_borrar = lista; 
+    // 4. Recorrer la lista.
+        while((aux_borrar != NULL) && (aux_borrar-> dato != n) ){ 
+            anterior = aux_borrar; 
+            aux_borrar = aux_borrar-> siguiente; 
+            }
+    // 5. Eliminar el elemento
+        if(aux_borrar == NULL){ 
+            cout<<"El elemento no existe."<<endl; 
+        }
+        else if(anterior == NULL){ 
+            lista = lista-> siguiente; 
+            delete aux_borrar;
+            cout<<"Elemento "<<n<<" ha sido eliminado correctamente"<<endl;
+        }else { 
+            anterior->siguiente = aux_borrar -> siguiente; 
+            delete aux_borrar;
+            cout<<"Elemento "<<n<<" ha sido eliminado correctamente"<<endl;
+        }
+    }
+
+
+
+}
 
 void menu(){ 
 
     int opcion, dato; 
     do{
-    cout<<"\t Menu"<<endl<<"\t 1. Insertar elementos en la lista"<<"\n\t 2. Mostrar los elementos en la lista"<<"\n\t 3. Buscar elemento"<<"\n\t 4. Salir"<<endl; 
+    cout<<"\t Menu"<<endl<<"\t 1. Insertar elementos en la lista"<<"\n\t 2. Mostrar los elementos en la lista"<<"\n\t 3. Buscar elemento"<<"\n\t 4. Borrar un elemento"<<"\n\t 5. Eliminar lista"<<"\n\t 6. Salir"<<endl; 
     cout<<"Digite la opcion que desea: "<<endl; cin>>opcion;
         switch(opcion){ 
             case 1: 
@@ -85,9 +128,22 @@ void menu(){
                 cin>>dato;
                 buscarElemento(lista,dato); 
             break;
-            case 4: break;
+            case 4: 
+                cout<<"Digite el dato a eliminar:"<<endl; 
+                cin>>dato;
+                eliminarElemento(lista,dato); 
+            break;
+            case 5: 
+                while(lista != NULL){ 
+                    eliminarLista(lista,dato); 
+                    cout<<dato<<" -> ";
+                }
+                cout<<"\n";
+                cout<<"Se eliminaron los elementos"<<endl;
+            break;
+            case 6: break;
         }
-    }while(opcion !=4); 
+    }while(opcion !=6); 
 }
 
 int main()
